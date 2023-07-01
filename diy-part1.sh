816d0454a875e69f9c2985bf7b1e282732b46bad
zow2023 <125123077+zow2023@users.noreply.github.com>
2023-07-01 09:44:09 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,4 +19,5 @@
 #git clone -b luci https://github.com/pexcn/openwrt-chinadns-ng.git package/luci-app-chinadns-ng
 #git clone https://github.com/pexcn/openwrt-chinadns-ng.git package/chinadns-ng
 #git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-#git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
