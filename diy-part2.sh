214bb8727d6c2af3bb2943ccb09047fac3a74718
zow2023 <125123077+zow2023@users.noreply.github.com>
2024-04-11 09:18:13 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,8 +10,11 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 sed -i 's/ +libopenssl-legacy//g' feeds/helloworld/shadowsocksr-libev/Makefile
 #
-git clone https://github.com/gngpp/luci-theme-design.git -b js package/luci-theme-design
-git clone https://github.com/gngpp/luci-app-design-config.git package/luci-app-design-config
+#git clone https://github.com/gngpp/luci-theme-design.git -b js package/luci-theme-design
+#git clone https://github.com/gngpp/luci-app-design-config.git package/luci-app-design-config
+git clone https://github.com/kingyond/luci-app-accesscontrol-plus package/luci-app-accesscontrol-plus
+
+rm -rf feeds/packages/net/xray-core
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 #git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
