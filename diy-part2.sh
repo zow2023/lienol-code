59cbf065ee8f33f335ef18ad5d9654a6c14947a2
zow2023 <125123077+zow2023@users.noreply.github.com>
2024-07-26 12:07:00 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -39,3 +39,5 @@ git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 #git clone https://github.com/sbwml/packages_lang_golang -b 21.x feeds/packages/lang/golang
 # Modify default IP
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+# Modify hostname
+sed -i 's/OpenWrt/E8450/g' package/base-files/files/bin/config_generate
