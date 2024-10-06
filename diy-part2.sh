c8c27b913d4577dc457f5048f522dabbcdd599a8
zow2023 <125123077+zow2023@users.noreply.github.com>
2024-10-07 00:02:38 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -21,7 +21,7 @@
 #git clone https://github.com/gSpotx2f/luci-app-cpu-perf package/luci-app-cpu-perf
 #git clone https://github.com/gSpotx2f/luci-app-interfaces-statistics package/luci-app-interfaces-statistics
 #git clone https://github.com/gSpotx2f/luci-app-temp-status package/luci-app-temp-status
-#git clone https://github.com/muink/luci-app-tinyfilemanager package/luci-app-tinyfilemanager
+git clone https://github.com/muink/luci-app-tinyfilemanager package/luci-app-tinyfilemanager
 #git clone https://github.com/muink/luci-app-tn-netports package/luci-app-tn-netports
 
 #rm -rf feeds/packages/net/chinadns-ng
