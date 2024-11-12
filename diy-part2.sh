8b1c45ff33186d4211f819b48deaff74e3a52970
zow2023 <125123077+zow2023@users.noreply.github.com>
2024-11-12 17:46:19 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,14 +18,14 @@
 #git clone https://github.com/animegasan/luci-app-quickstart package/luci-app-quickstart
 
 #git clone https://github.com/gSpotx2f/luci-app-cpu-status package/luci-app-cpu-status
-# git clone https://github.com/gSpotx2f/luci-app-cpu-perf package/luci-app-cpu-perf
+git clone https://github.com/gSpotx2f/luci-app-cpu-perf package/luci-app-cpu-perf
 #git clone https://github.com/gSpotx2f/luci-app-interfaces-statistics package/luci-app-interfaces-statistics
-# git clone https://github.com/gSpotx2f/luci-app-temp-status package/luci-app-temp-status
+git clone https://github.com/gSpotx2f/luci-app-temp-status package/luci-app-temp-status
 git clone https://github.com/muink/luci-app-tinyfilemanager package/luci-app-tinyfilemanager
-# git clone https://github.com/muink/luci-app-tn-netports package/luci-app-tn-netports
+git clone https://github.com/muink/luci-app-tn-netports package/luci-app-tn-netports
 # rm -rf feeds/other/lean/autocore-arm
 git clone https://github.com/xiaoxiao29/luci-app-adguardhome package/luci-app-adguardhome
-git clone https://github.com/sbwml/autocore-arm package/autocore-arm
+# git clone https://github.com/sbwml/autocore-arm package/autocore-arm
 git clone https://github.com/sbwml/luci-theme-argon package/luci-theme-argon
 git clone https://github.com/sirpdboy/luci-app-parentcontrol package/luci-app-parentcontrol
 
