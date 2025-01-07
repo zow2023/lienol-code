bf76f57f50153547524a27ff770c0ec43156c381
zow2023 <125123077+zow2023@users.noreply.github.com>
2025-01-07 19:03:11 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,11 +18,11 @@ git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-app-ar
 #git clone https://github.com/animegasan/luci-app-quickstart package/luci-app-quickstart
 
 #git clone https://github.com/gSpotx2f/luci-app-cpu-status package/luci-app-cpu-status
-# git clone https://github.com/gSpotx2f/luci-app-cpu-perf package/luci-app-cpu-perf
+git clone https://github.com/gSpotx2f/luci-app-cpu-perf package/luci-app-cpu-perf
 #git clone https://github.com/gSpotx2f/luci-app-interfaces-statistics package/luci-app-interfaces-statistics
-# git clone https://github.com/gSpotx2f/luci-app-temp-status package/luci-app-temp-status
+git clone https://github.com/gSpotx2f/luci-app-temp-status package/luci-app-temp-status
 # git clone https://github.com/muink/luci-app-tinyfilemanager package/luci-app-tinyfilemanager
-# git clone https://github.com/muink/luci-app-tn-netports package/luci-app-tn-netports
+git clone https://github.com/muink/luci-app-tn-netports package/luci-app-tn-netports
 # rm -rf feeds/other/lean/autocore-arm
 git clone https://github.com/xiaoxiao29/luci-app-adguardhome package/luci-app-adguardhome
 git clone https://github.com/sbwml/luci-app-filemanager package/luci-app-filemanager
@@ -55,6 +55,7 @@ git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/pas
 
 rm -rf feeds/packages/lang/node
 git clone https://github.com/sbwml/feeds_packages_lang_node-prebuilt -b packages-24.10 feeds/packages/lang/node
+curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
 
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
