fcf95d816d4fec0e4ae26bf2dedd8f31e557054d
zow2023 <125123077+zow2023@users.noreply.github.com>
2025-01-07 21:38:13 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -19,11 +19,14 @@ git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-app-ar
 
 #git clone https://github.com/gSpotx2f/luci-app-cpu-status package/luci-app-cpu-status
 git clone https://github.com/gSpotx2f/luci-app-cpu-perf package/luci-app-cpu-perf
-#git clone https://github.com/gSpotx2f/luci-app-interfaces-statistics package/luci-app-interfaces-statistics
-git clone https://github.com/gSpotx2f/luci-app-temp-status package/luci-app-temp-status
-# git clone https://github.com/muink/luci-app-tinyfilemanager package/luci-app-tinyfilemanager
 git clone https://github.com/muink/luci-app-tn-netports package/luci-app-tn-netports
+git clone https://github.com/gSpotx2f/luci-app-temp-status package/luci-app-temp-status
+#git clone https://github.com/gSpotx2f/luci-app-interfaces-statistics package/luci-app-interfaces-statistics
+# git clone https://github.com/muink/luci-app-tinyfilemanager package/luci-app-tinyfilemanager
+
+rm -rf feeds/other/lean/luci-app-turboacc
 # rm -rf feeds/other/lean/autocore-arm
+
 git clone https://github.com/xiaoxiao29/luci-app-adguardhome package/luci-app-adguardhome
 git clone https://github.com/sbwml/luci-app-filemanager package/luci-app-filemanager
 # git clone https://github.com/sbwml/autocore-arm package/autocore-arm
